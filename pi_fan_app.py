87566e433bd1b5b360a9f6b38ca18ef7a29aa5ea
Kirill Matyunin <Kirill.Matyunin@coxautoinc.com>
2021-01-04 16:56:01 -0500

    Fixing enabling auto mode

--- pi_fan_app.py
+++ pi_fan_app.py
@@ -75,8 +75,9 @@ def get_avg_temp():
 
 @app.route("/pwm/enable")
 def pwm_enable():
-    if not redis_client.pwm_enabled:
-        mode = request.args.get('mode', default=app.config["MANUAL_MODE"], type=str)
+    mode = request.args.get('mode', default=app.config["MANUAL_MODE"], type=str)
+    current_ctrl_mode = redis_client.current_ctrl_mode
+    if not redis_client.pwm_enabled or (redis_client.pwm_enabled and mode != current_ctrl_mode):
         redis_client.set_value(app.config["NEW_PWM_ENABLED"], True)
         redis_client.set_value(app.config["NEW_CTRL_MODE"], mode)
         redis_client.set_value(app.config["NEW_PWM_DUTY"], app.config["PWM_DEFAULT_DUTY"])
