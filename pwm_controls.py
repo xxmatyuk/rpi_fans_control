9eda9c9c5f83a77d3f63341651534cc773029f04
Kirill Matyunin <Kirill.Matyunin@coxautoinc.com>
2021-03-20 10:15:24 -0400

    Code clean-up

--- pwm_controls.py
+++ pwm_controls.py
@@ -71,17 +71,16 @@ def run_pwm_controls():
                     lights = GPIO.PWM(settings.LIGHTS_PIN, settings.LIGHT_DEFAULT_FREQ)
                 lights.start(settings.LIGHTS_PWM_DEFAULT)
                 continue
-        
+
         # PWM state has changed
         if pwm_enabled != new_pwm_enabled:
+            redis_client.set_value(settings.PWM_ENABLED, new_pwm_enabled)
             if new_pwm_enabled == False:
                 if fans:
                     fans.stop()
                 GPIO.cleanup()
-                redis_client.set_value(settings.PWM_ENABLED, False)
                 fans = None
             else:
-                redis_client.set_value(settings.PWM_ENABLED, True)
                 GPIO.setmode(GPIO.BCM)
                 GPIO.setup(settings.FANS_PIN, GPIO.OUT, initial=GPIO.LOW)
                 try:
@@ -90,52 +89,44 @@ def run_pwm_controls():
                     logger.error(str(e))
                 fans.start(settings.PWM_DEFAULT_DUTY)
                 continue
-        
-        # PWM has not changed and it's disabled, do nothing
+
+        # PWM is disabled, no need to go down below
         if pwm_enabled == False:
             continue
-            
+
         # PWM is enabled and control mode has changed
         if curr_ctrl_mode != new_ctrl_mode:
-            if new_ctrl_mode == settings.MANUAL_MODE:
-                redis_client.set_value(settings.CURR_CTRL_MODE, settings.MANUAL_MODE)
-                continue
-            else:
-                redis_client.set_value(settings.CURR_CTRL_MODE, settings.AUTO_MODE)
-                continue
-            
+            redis_client.set_value(settings.CURR_CTRL_MODE, new_ctrl_mode)
+
         # Manual mode
-        if curr_ctrl_mode == settings.MANUAL_MODE:
-            if curr_pwm_duty == new_pwm_duty:
+        if curr_ctrl_mode == settings.MANUAL_MODE and curr_pwm_duty != new_pwm_duty:
+            redis_client.set_value(settings.CURR_PWM_DUTY, new_pwm_duty)
+            if fans:
+                fans.ChangeDutyCycle(new_pwm_duty)
                 continue
-            else:
-                if fans:
-                    fans.ChangeDutyCycle(new_pwm_duty)
-                    redis_client.set_value(settings.CURR_PWM_DUTY, new_pwm_duty)
-                    continue
-        
+
         # Auto mode
-        if curr_ctrl_mode == settings.AUTO_MODE:
-            if curr_t1_temp and curr_t2_temp:
-                avg_temp = (curr_t1_temp + curr_t2_temp)/2
-                if fans:
-                    if avg_temp > curr_temp_threshold+2:
-                        fans.ChangeDutyCycle(100)
-                        redis_client.set_value(settings.CURR_PWM_DUTY, 100)
-                        continue
-                    elif avg_temp > curr_temp_threshold+1:
-                        duty = settings.PWM_DEFAULT_DUTY + 5
-                        fans.ChangeDutyCycle(duty)
-                        redis_client.set_value(settings.CURR_PWM_DUTY, duty)
-                        continue
-                    elif avg_temp > curr_temp_threshold:
-                        fans.ChangeDutyCycle(settings.PWM_DEFAULT_DUTY)
-                        redis_client.set_value(settings.CURR_PWM_DUTY, settings.PWM_DEFAULT_DUTY)
-                        continue
-                    else:
-                        fans.ChangeDutyCycle(0)
-                        redis_client.set_value(settings.CURR_PWM_DUTY, 0)
-                        continue
+        if curr_ctrl_mode == settings.AUTO_MODE and curr_t1_temp and curr_t2_temp:
+            avg_temp = (curr_t1_temp + curr_t2_temp)/2
+            if fans:
+                if avg_temp > curr_temp_threshold + 2:
+                    fans.ChangeDutyCycle(100)
+                    redis_client.set_value(settings.CURR_PWM_DUTY, 100)
+                    continue
+                elif avg_temp > curr_temp_threshold+1:
+                    duty = settings.PWM_DEFAULT_DUTY + 5
+                    fans.ChangeDutyCycle(duty)
+                    redis_client.set_value(settings.CURR_PWM_DUTY, duty)
+                    continue
+                elif avg_temp > curr_temp_threshold:
+                    fans.ChangeDutyCycle(settings.PWM_DEFAULT_DUTY)
+                    redis_client.set_value(settings.CURR_PWM_DUTY, settings.PWM_DEFAULT_DUTY)
+                    continue
+                else:
+                    fans.ChangeDutyCycle(0)
+                    redis_client.set_value(settings.CURR_PWM_DUTY, 0)
+                    continue
+
 
 # Do a clean-up
 atexit.register(_stop_pwm_control)
